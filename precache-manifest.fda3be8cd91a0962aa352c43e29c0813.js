852fc42925f08f53331592510d518fe8e7139c69
Lino Melhado <linomneto@gmail.com>
2021-12-19 02:32:13 +0000

    Updates

---
+++ precache-manifest.fda3be8cd91a0962aa352c43e29c0813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "336dddf5764bf17bdd4e",
+    "url": "/ethereum-marketplace/static/css/main.9a7a6ce8.chunk.css"
+  },
+  {
+    "revision": "336dddf5764bf17bdd4e",
+    "url": "/ethereum-marketplace/static/js/main.336dddf5.chunk.js"
+  },
+  {
+    "revision": "6289537ac007e4cfff5b",
+    "url": "/ethereum-marketplace/static/css/1.5ccd0503.chunk.css"
+  },
+  {
+    "revision": "6289537ac007e4cfff5b",
+    "url": "/ethereum-marketplace/static/js/1.6289537a.chunk.js"
+  },
+  {
+    "revision": "42e5a1b95d82815e13c3",
+    "url": "/ethereum-marketplace/static/js/runtime~main.42e5a1b9.js"
+  },
+  {
+    "revision": "b96857b2a8592d40d23368248bbb1f38",
+    "url": "/ethereum-marketplace/index.html"
+  }
+];
