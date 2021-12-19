18c26a39b4137779b361a1634cc1868d7eec8b4b
Lino Melhado Neto <linomneto@gmail.com>
2021-12-18 22:43:28 -0300

    ajuste

--- src/components/App.js
+++ src/components/App.js
@@ -74,7 +74,7 @@ class App extends Component {
       .on('error', console.error); // If a out of gas error, the second parameter is the receipt.
   }
   
-  purchaseProduct(id, price) {
+  purchaseProduct = (id, price) => {
     this.setState({ loading: true })
     this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
       .on('confirmation', function(confirmationNumber, receipt) {
@@ -83,7 +83,7 @@ class App extends Component {
       .on('error', console.error); // If a out of gas error, the second parameter is the receipt.
   }
 
-  editProductPrice(id, price) {
+  editProductPrice = (id, price) => {
     this.setState({ loading: true })
     this.state.marketplace.methods.editProductPrice(id, price).send({ from: this.state.account })
       .on('confirmation', function (confirmationNumber, receipt) {
