9b227784a93b4ba73f9caa0afa87721cdf8ccad7
Lino Melhado Neto <linomneto@gmail.com>
2021-12-18 22:56:11 -0300

    ajuste

--- src/components/Main.js
+++ src/components/Main.js
@@ -83,7 +83,7 @@ class Main extends Component {
                       name={product.id}
                       value={product.price}
                       onClick={(event) => {
-                        this.props.editProductPrice(product.id, this.newValueProduct)
+                        this.props.editProductPrice(product.id, this.newValueProduct.value)
                       }}
                     >Alterar valor</button>
 
