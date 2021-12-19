05bd995ce9dbcdb8995f1d377664e301c9fcc911
Lino Melhado Neto <linomneto@gmail.com>
2021-12-18 21:09:21 -0300

    ajuste

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -4,7 +4,7 @@ contract Marketplace {
   string public name;
   uint public productCount=0;
   mapping(uint => Product) public products;
-  address public owner;
+  address public contractOwner;
 
 
 struct Product {
@@ -32,7 +32,7 @@ event ProductPurchased (
 );
   constructor() public {
     name = "Dapp University Marketplace";
-    owner = msg.sender;
+    contractOwner = msg.sender;
   }
 
   function createProduct(string memory _name, uint _price) public {
@@ -70,7 +70,9 @@ event ProductPurchased (
     //Pay the seller by sending them Ether
     //address(_seller).transfer(msg.value);
     address(_seller).transfer((msg.value * 95) / 100);
-    address(owner).transfer((msg.value * 5) / 100);
+
+    address payable _owner = contractOwner;
+    address(_owner).transfer((msg.value * 5) / 100);
 
     //Trigger an event
     emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
